6ec708d23337c4de841cd72041e6aa5ae49a6a03
otpidusprime <sudiptomondal@pm.me>
2020-05-05 10:36:56 +0530

    Startpage thumbnail fancy shadow

--- custom.js
+++ custom.js
@@ -124,6 +124,9 @@ function cssm() {
     .startpage .startpage-navigation .startpage-navigation-group .button-startpage:hover {
         background: rgba(0,0,0,0.3);
     }
+    .thumbnail-image {
+        box-shadow: 0px 10px 50px -15px rgba(0,0,0,0.2) !important;
+    }
     button.button-startpage:after {
         display: none;
     }
